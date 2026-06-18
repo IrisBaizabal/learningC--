dba6931c79616d6390c3e8584ca09ffc612965de
Iris <a00843705@tec.mx>
2026-06-18 10:24:48 -0600

    Finished last exercise of vectors

---
+++ vectors/sum_product.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+int main(){
+    std::vector<int> num = {2, 4, 3, 6, 1, 9, 8, 0, 8, 5, 7, 3, 3};
+    int sum = 0;
+    int product = 1;
+    for (int i=0; i<num.size(); i++){
+        if(num[i]%2 == 0){
+            sum += num[i];
+        }else{
+            product = product * num[i];
+        }
+    }
+
+    std::cout << "The sum of even numbers: " << sum << std::endl;
+    std::cout << "The product of odd numbers: " << product << std::endl;
+
+    return 0;
+}
