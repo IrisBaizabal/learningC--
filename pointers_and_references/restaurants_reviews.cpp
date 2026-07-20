027b709f148b4817adb846d1d96f7e89b5461b88
Iris <a00843705@tec.mx>
2026-07-20 11:22:12 -0600

    Adding the other use of the function

--- pointers_and_references/restaurants_reviews.cpp
+++ pointers_and_references/restaurants_reviews.cpp
@@ -4,10 +4,12 @@ void reviewAction(const std::string &name, const std::string* extraCommment){
     if (extraCommment == nullptr){
         std::cout<< "Thanks for the review, "<< name << std::endl;
     } else{
-        std::cout<<"Thanks for the review, "<< name <<" The team will get back to you if you had any questions.\n";
+        std::cout<<"Thanks for the review, "<< name <<"! The team will get back to you if you had any questions.\n";
     }
 }
 int main(){
+    std::string review = "The food was delicious";
     reviewAction("Maria", nullptr);
+    reviewAction("Maria", &review);
     return 0;
 }
